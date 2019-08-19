c2beabb206f052c4d43d5b9963a79a0d1a7a1d6f
Kyle Surowiec <kyle.surowiec@perficient.com>
2019-08-19 20:59:53 +0100

    cleaner examples

--- lib/examples.ex
+++ lib/examples.ex
@@ -1,24 +1,26 @@
 defmodule Examples do
   def main() do
-    film_names =
-      Swapi.get_all_films()
-      |> Enum.map(& &1["title"])
+    # Output:
+    # A New Hope
+    # Attack of the Clones
+    # The Phantom Menace
+    # ...
+    Swapi.get_all_films()
+    |> Enum.each(fn f -> IO.puts("#{f["title"]}") end)
 
-    # ["A New Hope", "Attack of the Clones", ...]
-    IO.inspect(film_names)
+    # Output:
+    # Alderaan
+    # Yavin IV
+    # Hoth
+    # ...
+    Swapi.get_all_planets()
+    |> Enum.each(fn p -> IO.puts("#{p["name"]}") end)
 
-    planet_names =
-      Swapi.get_all_planets()
-      |> Enum.map(& &1["name"])
-
-    # ["Alderaan", "Yavin IV", "Hoth", ...]
-    IO.inspect(planet_names)
-
-    languages =
-      Swapi.get_all_species()
-      |> Enum.map(&"#{&1["name"]}: #{&1["language"]}")
-
-    # ["Hutt: Huttese", "Yoda's species: Galactic basic", "Trandoshan: Dosh", ...]
-    IO.inspect(languages)
+    # Output:
+    # Hutt: Huttese
+    # Yoda's species: Galactic basic
+    # Trandoshan: Dosh
+    Swapi.get_all_species()
+    |> Enum.each(fn s -> IO.puts("#{s["name"]}: #{s["language"]}") end)
   end
 end
